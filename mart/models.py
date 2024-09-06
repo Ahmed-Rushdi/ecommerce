8ef79767d05a6af4dafcc36e5510479e86d6a579
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-09-06 23:14:44 +0300

    finisehed integrating product-model

--- mart/models.py
+++ mart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    image_url = models.CharField(max_length=2083)
+    is_available = models.BooleanField()
+
+    def __str__(self):
+        return self.name
